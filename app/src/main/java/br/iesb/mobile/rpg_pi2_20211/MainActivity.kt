447c971ce301828dada093224bdb90ab67ae9130
Emanuel Felipe <emanuelfelipe0501@gmail.com>
2021-03-11 18:40:43 -0300

    primeiro commit inicio projeto

---
+++ app/src/main/java/br/iesb/mobile/rpg_pi2_20211/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.iesb.mobile.rpg_pi2_20211
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
