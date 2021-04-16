4bc01856bef099e0d232ef23c1eacd7fb9e67d88
Isabella <ricciisa05@gmail.com>
2021-04-15 21:28:50 -0300

    teste onboarding

---
+++ app/src/main/java/br/iesb/mobile/rpg_pi2_20211/ui/fragment/onboarding/OnboardingFragment.kt
@@ -0,0 +1,30 @@
+package br.iesb.mobile.rpg_pi2_20211.ui.fragment.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.iesb.mobile.rpg_pi2_20211.R
+
+
+class OnboardingFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+    }
+
+
+}
+
---
+++ app/src/main/java/br/iesb/mobile/rpg_pi2_20211/ui/fragment/onboarding/screen/OnboardingFirstFragment.kt
@@ -0,0 +1,23 @@
+package br.iesb.mobile.rpg_pi2_20211.ui.fragment.onboarding.screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.iesb.mobile.rpg_pi2_20211.R
+
+
+class OnboardingFirstFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding_first, container, false)
+    }
+
+
+}
