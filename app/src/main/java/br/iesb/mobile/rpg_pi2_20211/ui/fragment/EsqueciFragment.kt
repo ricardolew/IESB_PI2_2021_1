c69606e4c61bedeebed2eee8d5362c161bf5eac8
Gabriel Rodrigues <rsgabriel.sousa@gmail.com>
2021-04-18 18:49:16 -0300

    Modificação Activity - Fragment (Esqueci e Personagem)

--- app/src/main/java/br/iesb/mobile/rpg_pi2_20211/ui/fragment/EsqueciFragment.kt
+++ app/src/main/java/br/iesb/mobile/rpg_pi2_20211/ui/fragment/EsqueciFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import br.iesb.mobile.rpg_pi2_20211.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_esqueci.*
 
 class EsqueciFragment : Fragment() {
 
@@ -21,4 +24,38 @@ class EsqueciFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_esqueci, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+            btEnviarEmail.setOnClickListener {
+            val email = etEmailEsqueci.text.toString()
+
+            val auth = FirebaseAuth.getInstance()
+            if (email.isEmpty()) {
+                Toast.makeText(activity, "Insira o email", Toast.LENGTH_LONG).show()
+            }else{
+
+
+            val taskEsqueci = auth.sendPasswordResetEmail(email)
+                taskEsqueci.addOnCompleteListener { resultado ->
+                    if (resultado.isSuccessful) {
+                        Toast.makeText(activity, "E-mail de recuperação enviado! Favor verificar o correio eletrônico", Toast.LENGTH_LONG).show()
+
+                        activity?.finish()
+                    } else {
+                        Toast.makeText(activity, "Email inválidos", Toast.LENGTH_LONG).show()
+                    }
+                }
+//            Toast.makeText(this, "E-mail de recuperação enviado! Favor verificar o correio eletrônico", Toast.LENGTH_LONG).show()
+//            finish()
+        }
+
+        }
+        btVoltarEsqueci.setOnClickListener {
+
+            activity?.finish()
+        }
+    }
+
 }
--- app/src/main/java/br/iesb/mobile/rpg_pi2_20211/ui/fragment/PersonagemFragment.kt
+++ app/src/main/java/br/iesb/mobile/rpg_pi2_20211/ui/fragment/PersonagemFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.iesb.mobile.rpg_pi2_20211.R
+import kotlinx.android.synthetic.main.fragment_personagem.*
 
 class PersonagemFragment : Fragment() {
 
@@ -20,6 +21,26 @@ class PersonagemFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_personagem, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btCharInfos.setOnClickListener {
+
+        }
+        btCharSpells.setOnClickListener {
+
+        }
+        btCharInv.setOnClickListener {
+
+        }
+        btCharTaverna.setOnClickListener {
+
+        }
+        btCharMap.setOnClickListener {
+
+        }
     }
 
 }
